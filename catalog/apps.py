4b78ecc637b7527c9f0f69b27e4dc3eebd898423
Kirill Gladkikh <k.gladkikh@mail.ru>
2025-12-04 14:01:38 +0300

    app catalog contacts.html views urls - add

--- catalog/apps.py
+++ catalog/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
 
 
+class NewappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'catalog'
+
+
 class CatalogConfig(AppConfig):
     name = "catalog"
