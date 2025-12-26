407caa5b522ff4b97d3f82fd409426d72c04a8a3
kirill.gladkikh <k.gladkikh@mail.ru>
2025-12-27 00:45:26 +0300

    app catalog media settings done

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("catalog.urls", namespace="catalog"))]
+urlpatterns = [path("admin/", admin.site.urls), path("", include("catalog.urls", namespace="catalog"))] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
