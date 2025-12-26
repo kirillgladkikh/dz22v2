7b84da2b4fcad83f110631495f8c45fec9566b09
kirill.gladkikh <k.gladkikh@mail.ru>
2025-12-27 00:56:17 +0300

    linted

--- config/settings.py
+++ config/settings.py
@@ -130,7 +130,9 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]  # Квадратные скобки обязательны, так как это список путей.
 
 MEDIA_URL = "media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # классический и надёжный вариант для MEDIA_ROOT. Подходит для старых версий Python/Django.
+MEDIA_ROOT = os.path.join(
+    BASE_DIR, "media"
+)  # классический и надёжный вариант для MEDIA_ROOT. Подходит для старых версий Python/Django.
 # MEDIA_ROOT = BASE_DIR / "media"  # современный и предпочтительный синтаксис (если проект использует pathlib). Эквивалентен os.path.join, но чище.
 
 # Default primary key field type
