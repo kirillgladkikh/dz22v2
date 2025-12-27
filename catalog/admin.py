3b873beee5dcec98579ae795f5f669c355acae98
kirill.gladkikh <k.gladkikh@mail.ru>
2025-12-27 18:42:29 +0300

    base.html w all product cards done

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,7 +5,7 @@ from catalog.models import Product, Category
 # Register your models here.
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("id", "product_name", "product_price", "product_category")
+    list_display = ("id", "product_name", "product_price", "product_category", "product_description", "product_image")
     list_filter = ("product_category",)
     search_fields = (
         "product_name",
