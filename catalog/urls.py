4bc78376cace696fc7dcae31ffd68c8420ed4408
kirill.gladkikh <k.gladkikh@mail.ru>
2025-12-28 01:39:32 +0300

    linted

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,9 +5,7 @@ from catalog.views import product_card, contacts, products_list  # index, home,
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    # path("", home, name="home"),
+    path("", products_list, name="products_list"),
     path("catalog/<int:pk>/", product_card, name="product_card"),
     path("contacts/", contacts, name="contacts"),
-    # path("", index),
-    path("", products_list, name="products_list"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -11,20 +11,7 @@ def products_list(request):
         "products": products,
         "is_product_card": False,  # Флаг: это НЕ детальная страница = НЕ product_card.html
     }
-    # products = Product.objects.all()
-    # context = {"products": products}
     return render(request, "products_list.html", context)
-
-
-# def home(request):
-#     """Список товаров"""
-#     products = Product.objects.all()
-#     context = {
-#         'products': products,
-#         'is_product_card': False,  # Флаг: это НЕ детальная страница = НЕ product_card.html
-#     }
-#     # context = {"products": products}
-#     return render(request, "home.html", context)
 
 
 def product_card(request, pk):
@@ -37,35 +24,8 @@ def product_card(request, pk):
     return render(request, "product_card.html", context)
 
 
-# def product_list(request):
-#     """Главная страница — список товаров"""
-#     products = Product.objects.all()
-#     context = {
-#         'products': products,
-#         'is_detail_page': False,  # Флаг: это НЕ детальная страница
-#     }
-#     return render(request, 'myapp/product_list.html', context)
-#
-# def product_detail(request, pk):
-#     """Детальная страница товара"""
-#     product = get_object_or_404(Product, pk=pk)
-#     context = {
-#         'product': product,
-#         'is_detail_page': True,   # Флаг: это детальная страница
-#     }
-#     return render(request, 'myapp/product_detail.html', context)
-
-
-# def index(request):
-#     return render(request, "base.html")
-
-
-# def home(request):
-#     return render(request, "home.html")
-
-
 def contacts(request):
-    # return render(request, 'contacts.html')
+    """Отправка контактов"""
     if request.method == "POST":
         name = request.POST.get("name")
         phone = request.POST.get("phone")
