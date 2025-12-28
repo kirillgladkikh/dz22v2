3f54404a16486fbfe3009c79cf5889ea83eef77f
kirill.gladkikh <k.gladkikh@mail.ru>
2025-12-28 18:01:56 +0300

    controllers from FBV to CBV done

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,23 +6,7 @@ from .views import ProductsListView, ProductCardView, ContactsView
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', ProductsListView.as_view(), name='products_list'),
-    path('product/<int:pk>/', ProductCardView.as_view(), name='product_card'),
-    path('contacts/', ContactsView.as_view(), name='contacts'),
+    path("", ProductsListView.as_view(), name="products_list"),
+    path("product/<int:pk>/", ProductCardView.as_view(), name="product_card"),
+    path("contacts/", ContactsView.as_view(), name="contacts"),
 ]
-
-
-
-
-
-# from django.urls import path
-# from catalog.apps import CatalogConfig
-# from catalog.views import product_card, contacts, products_list  # index, home,
-#
-# app_name = CatalogConfig.name
-#
-# urlpatterns = [
-#     path("", products_list, name="products_list"),
-#     path("catalog/<int:pk>/", product_card, name="product_card"),
-#     path("contacts/", contacts, name="contacts"),
-# ]
--- catalog/views.py
+++ catalog/views.py
@@ -5,7 +5,6 @@ from django.http import HttpResponse
 from catalog.models import Product
 
 
-
 class ProductsListView(TemplateView):
     template_name = "products_list.html"
 
@@ -27,7 +26,6 @@ class ProductCardView(DetailView):
         return context
 
 
-
 class ContactsView(View):
     template_name = "contacts.html"
 
@@ -43,51 +41,3 @@ class ContactsView(View):
             Ваш телефон: {phone}. \
             Сообщение получено: {message}."
         )
-
-
-
-
-
-
-
-
-# from django.shortcuts import render, get_object_or_404
-# from django.http import HttpResponse
-# from catalog.models import Product
-#
-#
-# # Create your views here.
-# def products_list(request):
-#     """Список товаров"""
-#     products = Product.objects.all()
-#     context = {
-#         "products": products,
-#         "is_product_card": False,  # Флаг: это НЕ детальная страница = НЕ product_card.html
-#     }
-#     return render(request, "products_list.html", context)
-#
-#
-# def product_card(request, pk):
-#     """Детальная страница товара"""
-#     product = get_object_or_404(Product, pk=pk)
-#     context = {
-#         "product": product,
-#         "is_product_card": True,  # Флаг: это детальная страница = product_card.html !
-#     }
-#     return render(request, "product_card.html", context)
-#
-#
-# def contacts(request):
-#     """Отправка контактов"""
-#     if request.method == "POST":
-#         name = request.POST.get("name")
-#         phone = request.POST.get("phone")
-#         message = request.POST.get("message")
-#
-#         return HttpResponse(
-#             f"Спасибо, {name}! \
-#             Ваш телефон: {phone}. \
-#             Сообщение получено: {message}."
-#         )
-#
-#     return render(request, "contacts.html")
