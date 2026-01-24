97dd898cb9c9fc1436f8ca92a345cba74cc97b48
kirill.gladkikh <k.gladkikh@mail.ru>
2026-01-24 16:33:33 +0300

    Задание 2 login/logout done w django 4.2.2

--- user/urls.py
+++ user/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from user.apps import UserConfig
 from . import views
@@ -8,7 +8,9 @@ app_name = UserConfig.name
 
 urlpatterns = [
     path('', views.user, name='user'),  # пример маршрута
-    path('login/', LoginView.as_view(template_name="login.html")),
+    # path('login/', LoginView.as_view(template_name="login.html")),
+    path('login/', LoginView.as_view(template_name="login.html"), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
 
 
