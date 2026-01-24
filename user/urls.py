3afc13320482f368478cbaf861104c7cbc1e7a40
kirill.gladkikh <k.gladkikh@mail.ru>
2026-01-24 16:58:40 +0300

    Задание 2 register done

--- user/urls.py
+++ user/urls.py
@@ -1,16 +1,18 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from user.apps import UserConfig
+from user.views import UserCreateView
 from . import views
 
 
 app_name = UserConfig.name
 
 urlpatterns = [
-    path('', views.user, name='user'),  # пример маршрута
+    # path('', views.user, name='user'),  # пример маршрута
     # path('login/', LoginView.as_view(template_name="login.html")),
     path('login/', LoginView.as_view(template_name="login.html"), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', UserCreateView.as_view(), name='register'),
 ]
 
 
--- user/views.py
+++ user/views.py
@@ -1,5 +1,16 @@
-from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from user.forms import UserRegisterForm
+from user.models import User
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('user:login')
 
 
-def user(request):
-    return HttpResponse("Привет из приложения user!")
+# from django.http import HttpResponse
+#
+#
+# def user(request):
+#     return HttpResponse("Привет из приложения user!")
