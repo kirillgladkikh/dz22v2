cf717af7dad462bfd07c7205ac121ed85027b721
kirill.gladkikh <k.gladkikh@mail.ru>
2026-01-24 17:14:44 +0300

    Задание 2 register & login forms styled by StyleFormMixin

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from user.apps import UserConfig
 from user.views import UserCreateView
+from user.forms import LoginForm
 from . import views
 
 
@@ -10,23 +11,8 @@ app_name = UserConfig.name
 urlpatterns = [
     # path('', views.user, name='user'),  # пример маршрута
     # path('login/', LoginView.as_view(template_name="login.html")),
-    path('login/', LoginView.as_view(template_name="login.html"), name='login'),
+    # path('login/', LoginView.as_view(template_name="login.html"), name='login'),
+    path('login/', LoginView.as_view(authentication_form=LoginForm, template_name="login.html"), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', UserCreateView.as_view(), name='register'),
 ]
-
-
-
-
-
-
-
-
-# пример
-# urlpatterns = [
-#     path("", views.BlogPostListView.as_view(), name="list"),
-#     path("create/", views.BlogPostCreateView.as_view(), name="create"),
-#     path("<int:pk>/", views.BlogPostDetailView.as_view(), name="detail"),
-#     path("<int:pk>/update/", views.BlogPostUpdateView.as_view(), name="update"),
-#     path("<int:pk>/delete/", views.BlogPostDeleteView.as_view(), name="delete"),
-# ]
